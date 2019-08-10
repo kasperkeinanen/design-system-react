16d251a0f3725cf6ff1ebb1c59ec027a45ae9ae4
Kasper Keinänen <kasper@Kaspers-MacBook-Pro.local>
2019-08-10 13:58:37 +0300

    Removed cra public/ folder and created prod build for styleguidist.

---
+++ styleguide/precache-manifest.c3c3f2a9c47c447bc2356d8da6a44e4b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a03b6da98349a47b8824",
+    "url": "build/2.a03b6da9.js"
+  },
+  {
+    "revision": "feb19782ca779508500c",
+    "url": "build/bundle.feb19782.js"
+  },
+  {
+    "revision": "966566d17c7056f3f1bb",
+    "url": "build/main.966566d1.js"
+  },
+  {
+    "revision": "7dfd31f318f47ba75b9911542549e88d",
+    "url": "index.html"
+  }
+]);
