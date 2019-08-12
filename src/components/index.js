725b8e7ec86b142f6389b43b5ac0e6b5956e0ea7
Kasper Keinänen <kasper.keinanen@aalto.fi>
2019-08-12 17:24:10 +0300

    Added index.js files to components and src

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { Button } from './Button/button.jsx'
+export { ContentSwitcher } from './ContentSwitcher/contentSwitcher'
+export { TextButton } from './TextButton/textButton'
+export { Tooltip } from './Tooltip/tooltip'
